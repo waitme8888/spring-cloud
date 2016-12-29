1cecf4b61e934afc7fffffdf37a382edb29cfa17
waitme8888 <18601144707@163.com>
2016-12-29 14:27:54 +0800

    none

--- spring-cloud-netflix-ribbon/src/main/java/com/waitme/cloud/eureka/Application.java
+++ spring-cloud-netflix-ribbon/src/main/java/com/waitme/cloud/eureka/Application.java
@@ -1,10 +1,11 @@
 package com.waitme.cloud.eureka;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class Application {
 
     @Bean
@@ -30,9 +32,14 @@ public class Application {
     RestTemplate restTemplate;
 
     @RequestMapping("callEurekaService")
+//    @HystrixCommand(fallbackMethod="callServiceFail")
     public String callEurekaService() {
         String str = restTemplate.getForEntity("http://EUREKA-CLIENT-SERVICE/eurekaService", String.class).getBody();
         return str;
+    }
+
+    public String callServiceFail() {
+        return "call eureka service fail";
     }
 
     public static void main(String[] args) {
