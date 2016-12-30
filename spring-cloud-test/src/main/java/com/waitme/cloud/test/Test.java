0899463876e4581b70510d2f1129e7bc721ea476
waitme8888 <18601144707@163.com>
2016-12-30 17:49:54 +0800

    none

---
+++ spring-cloud-test/src/main/java/com/waitme/cloud/test/Test.java
@@ -0,0 +1,10 @@
+package com.waitme.cloud.test;
+
+/**
+ * Created by weiwang207 on 2016/12/30.
+ */
+public class Test {
+    public String say() {
+        return "hello!";
+    }
+}
---
+++ spring-cloud-web/src/main/java/com.waitme.cloud.web/Application.java
@@ -0,0 +1,17 @@
+package com.waitme.cloud.web;
+
+import com.waitme.cloud.test.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+
+        Test test = new Test();
+		System.out.println(test.say());
+	}
+
+}
